b611f24e654e00e84503a5608fd293a5b5dd6f3f
Pavel <pavel.ochkovkiy@gmail.com>
2018-02-11 22:18:04 +0300

    Change iblock module isset

--- 1c_delete_offers.php
+++ 1c_delete_offers.php
@@ -4,29 +4,30 @@
 	require_once($_SERVER['DOCUMENT_ROOT'] . "/bitrix/modules/main/include/prolog_before.php");
 	// удаляем OFFERS без сезона
 	
-	CModule::IncludeModule("iblock");
-	$last_id = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/bitrix/log/last__del_offer_id.txt');
-	$arSelect = Array("ID", "IBLOCK_ID", "PROPERTY_SEZON");
-	$arFilter = Array("IBLOCK_ID"=> 8, "PROPERTY_SEZON" => false, '>ID' => $last_id);
-	$res = CIBlockElement::GetList(Array('ID' => 'ASC'), $arFilter, false, Array("nPageSize"=>25), $arSelect);
-    $next = false;
-	while($ob = $res->GetNextElement())
-	{
-		$arFields = $ob->GetFields();
-		if(isset($arFields['PROPERTY_SEZON_VALUE']{0})) continue;
-		CIBlockElement::Delete($arFields["ID"]);
+	if(CModule::IncludeModule("iblock")){
+		$last_id = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/bitrix/log/last__del_offer_id.txt');
+		$arSelect = Array("ID", "IBLOCK_ID", "PROPERTY_SEZON");
+		$arFilter = Array("IBLOCK_ID"=> 8, "PROPERTY_SEZON" => false, '>ID' => $last_id);
+		$res = CIBlockElement::GetList(Array('ID' => 'ASC'), $arFilter, false, Array("nPageSize"=>25), $arSelect);
+		$next = false;
+		while($ob = $res->GetNextElement())
+		{
+			$arFields = $ob->GetFields();
+			if(isset($arFields['PROPERTY_SEZON_VALUE']{0})) continue;
+			CIBlockElement::Delete($arFields["ID"]);
+			
+			$last_id = $arFields['ID'];
+			$next = true;
+		}
+		echo $last_id;
+		file_put_contents($_SERVER['DOCUMENT_ROOT'].'/bitrix/log/last__del_offer_id.txt', $last_id);
 		
-		$last_id = $arFields['ID'];
-        $next = true;
-	}
-	echo $last_id;
-    file_put_contents($_SERVER['DOCUMENT_ROOT'].'/bitrix/log/last__del_offer_id.txt', $last_id);
-	
-	if($next){
-		//echo '<meta http-equiv="refresh" content="0; url="http://www.tribuna.com.ru/bitrix/1c_delete_offers.php">'; 
-		exec("php /d/www/t/tribuna.com.ru/docs/bitrix/1c_delete_offers.php");
-		}else{
-        file_put_contents($_SERVER['DOCUMENT_ROOT'].'/bitrix/log/last__del_offer_id.txt', '0');
-		die("THE END");	
+		if($next){
+			//echo '<meta http-equiv="refresh" content="0; url="http://www.tribuna.com.ru/bitrix/1c_delete_offers.php">'; 
+			exec("php /d/www/t/tribuna.com.ru/docs/bitrix/1c_delete_offers.php");
+			}else{
+			file_put_contents($_SERVER['DOCUMENT_ROOT'].'/bitrix/log/last__del_offer_id.txt', '0');
+			die("THE END");	
+		}
 	}
 ?>
